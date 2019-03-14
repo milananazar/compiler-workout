e4d429554c14c0d72f32eef5650b86dac406b276
milananazar <milana.nazarenkova@mail.ru>
2019-03-14 12:44:08 +0300

    4dz

--- src/Language.ml
+++ src/Language.ml
@@ -38,22 +38,63 @@ module Expr =
     let update x v s = fun y -> if x = y then v else s y
 
     (* Expression evaluator
-
           val eval : state -> t -> int
  
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
-     *)                                                       
-    let eval _ _ = failwith "Not yet implemented"
+    *)
+    let fromBool b = if b then 1 else 0
+
+    let toBool b = b <> 0 
+
+    let ($) f1 f2 a b = f2 (f1 a b)
+
+    let getFunction op = match op with 
+       | "+" -> (+)
+       | "-" -> (-)
+       | "*" -> ( * )
+       | "/" -> (/)
+       | "%" -> (mod)
+       | "<" -> (<) $ fromBool
+       | "<=" -> (<=) $ fromBool
+       | ">" -> (>) $ fromBool
+       | ">=" -> (>=) $ fromBool
+       | "==" -> (=) $ fromBool
+       | "!=" -> (<>) $ fromBool 
+       | "&&" -> fun x y -> fromBool ((&&) (toBool x) (toBool y)) 
+       | "!!" -> fun x y -> fromBool ((||) (toBool x) (toBool y))
+       | _ -> raise Not_found
+
+    let rec eval state expr =  match expr with
+       | Const a -> a
+       | Var x -> state x
+       | Binop (op, x, y) -> (getFunction op) 
+            (eval state x) (eval state y)
 
     (* Expression parser. You can use the following terminals:
-
          IDENT   --- a non-empty identifier a-zA-Z[a-zA-Z0-9_]* as a string
          DECIMAL --- a decimal constant [0-9]+ as a string
                                                                                                                   
     *)
-    ostap (                                      
-      parse: empty {failwith "Not yet implemented"}
+
+    let parseBinop op = ostap(- $(op)), (fun x y -> Binop(op, x, y))
+
+    ostap (
+      expr: 
+           !(Ostap.Util.expr
+                 (fun x -> x)
+                 (Array.map (fun (assoc, ops) -> assoc, List.map parseBinop ops)
+                  [|
+                     `Lefta, ["!!"];
+                     `Lefta, ["&&"];
+                     `Nona, ["<="; "<"; ">="; ">"; "=="; "!="];
+                     `Lefta, ["+"; "-"];
+                     `Lefta, ["*"; "/"; "%"];
+                  |]
+                 )
+                 primary
+             );
+      primary: n:DECIMAL {Const n} | x:IDENT {Var x} | -"(" expr -")"
     )
     
   end
@@ -73,16 +114,35 @@ module Stmt =
     type config = Expr.state * int list * int list 
 
     (* Statement evaluator
-
          val eval : config -> t -> config
-
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ _ = failwith "Not yet implemented"
+
+    (* Statement parser *)
+    (*ostap (
+      parse: empty {failwith "Not yet implemented"}
+ 
+          val eval : config -> t -> config
+       Takes a configuration and a statement, and returns another configuration
+    )
+*)
+    let rec eval (state, input, output) stmt = match stmt with
+        | Read x -> (match input with
+                    | head :: tail -> (Expr.update x head state, tail, output)
+                    | _ -> failwith ("Empty input")
+                    )
+        | Write e -> (state, input, output @ [Expr.eval state e])
+        | Assign (x, e) -> (Expr.update x (Expr.eval state e) state, input, output)
+        | Seq (stmt1, stmt2) -> eval (eval (state, input, output) stmt1) stmt2 
 
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not yet implemented"}
+      line: 
+          "read"  "(" x:IDENT ")"          {Read x}
+        | "write" "(" e:!(Expr.expr) ")"   {Write e}
+        | x:IDENT ":=" e:!(Expr.expr)     {Assign (x, e)};
+ 
+        parse: l:line ";" rest:parse {Seq (l, rest)} | line
     )
       
   end
@@ -93,13 +153,11 @@ module Stmt =
 type t = Stmt.t    
 
 (* Top-level evaluator
-
      eval : t -> int list -> int list
-
    Takes a program and its input stream, and returns the output stream
 *)
 let eval p i =
   let _, _, o = Stmt.eval (Expr.empty, i, []) p in o
 
 (* Top-level parser *)
-let parse = Stmt.parse                                                     
+let parse = Stmt.parse   
--- src/SM.ml
+++ src/SM.ml
@@ -19,25 +19,39 @@ type prg = insn list
 type config = int list * Stmt.config
 
 (* Stack machine interpreter
-
      val eval : config -> prg -> config
-
    Takes a configuration and a program, and returns a configuration as a result
-*)                         
-let rec eval conf prog = failwith "Not yet implemented"
+*)                                                  
+let evalRow (stack, (state, cfgInput, cfgOutput))  prg = match prg with
+  | BINOP op -> (match stack with
+                 | y::x::tail -> ([Language.Expr.getFunction op x y] @ tail, (state, cfgInput, cfgOutput))
+                 | _ -> failwith "Not enouth values on stack for BINOP"
+                 )
+  | CONST n -> ([n] @ stack, (state, cfgInput, cfgOutput))
+  | READ -> (match cfgInput with
+            | head :: tail -> ([head] @ stack, (state, tail, cfgOutput))
+            | _ -> failwith "Can't read from inputStream"
+            )
+  | WRITE -> (match stack with 
+             | head :: tail -> (tail, (state, cfgInput, cfgOutput @ [head]))
+             | _ -> failwith "Not enouth values on stack for WRITE"
+             )
+  | LD x -> ([state x] @ stack, (state, cfgInput, cfgOutput))
+  | ST x -> (match stack with 
+            | head::tail -> (tail, (Language.Expr.update x head state, cfgInput, cfgOutput))
+            | _ -> failwith "Not enouth values on stack for ST"
+            )
+
+let eval cfg prg = List.fold_left evalRow cfg prg
 
 (* Top-level evaluation
-
      val run : prg -> int list -> int list
-
    Takes a program, an input stream, and returns an output stream this program calculates
 *)
 let run p i = let (_, (_, _, o)) = eval ([], (Expr.empty, i, [])) p in o
 
 (* Stack machine compiler
-
      val compile : Language.Stmt.t -> prg
-
    Takes a program in the source language and returns an equivalent program for the
    stack machine
 *)
@@ -52,3 +66,14 @@ let rec compile =
   | Stmt.Read x        -> [READ; ST x]
   | Stmt.Write e       -> expr e @ [WRITE]
   | Stmt.Assign (x, e) -> expr e @ [ST x]
+(*
+let rec compileRow expr = match expr with
+  | Language.Expr.Const n -> [CONST n]
+  | Language.Expr.Var x -> [LD x]
+  | Language.Expr.Binop (op, a, b) -> (compileRow a) @ (compileRow b) @ [BINOP op]
+let rec compile stmt = match stmt with
+  | Language.Stmt.Read x -> [READ; ST x]
+  | Language.Stmt.Write e -> (compileRow e) @ [WRITE]
+  | Language.Stmt.Assign (x, e) -> (compileRow e) @ [ST x]
+  | Language.Stmt.Seq (stmt1, stmt2) -> (compile stmt1) @ (compile stmt2)
+*)
